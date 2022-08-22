3b05a71fbdd7eaca9b95357a122adaa12dd3157a
aisanaK <89066352+aisanaK@users.noreply.github.com>
2022-08-22 13:13:07 +0600

    Dash framework

---
+++ app.py
@@ -0,0 +1,57 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc, Input, Output
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import yfinance as yf
+
+
+app = Dash(__name__)
+colors = {
+    'background': '#000000',
+    'text': '#000000'
+}
+
+tsla = yf.Ticker("TSLA")
+# info = tsla.info
+tsla_data = tsla.history(period='1y')
+fig = go.Figure(data=go.Scatter(x=tsla_data.index, y=tsla_data["Close"]))
+
+
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Stocks',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }
+            ),
+
+    html.Div(children='''
+        Input a stock index
+    ''', style={'textAlign': 'center', 'color': colors['text']}),
+html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output'),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
